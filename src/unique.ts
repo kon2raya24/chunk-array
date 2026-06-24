6665fff810c16068841b9ee005ae4a911d8354f1
kon2raya24 <konturaya@gmail.com>
2026-06-24 10:58:58 +0000

    docs: add JSDoc comments to unique.ts

--- src/unique.ts
+++ src/unique.ts
@@ -1,3 +1,7 @@
+/**
+ * unique<T>
+ * @param arr
+ */
 export function unique<T>(arr: T[]): T[] {
   if (arr === null || arr === undefined) throw new Error("Invalid input");
   return [...new Set(arr)];
